b8ca8885970f112b501f24670476c4356a956678
Ilya Kuzmin <akalyig@mail.ru>
2022-07-06 18:30:44 +0300

    demo the weather api

---
+++ the-weather/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {WeatherDisplay} from "./components/WeatherDisplay";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherDisplay />
+    </div>
+  );
+}
+
+export default App;
